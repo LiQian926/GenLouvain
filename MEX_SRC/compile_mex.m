03aa5b08b58cd45903e8172fd34693eda9a192b8
LiQian926 <1571820809@qq.com>
2023-03-27 02:22:44 +0800

    generate mex files for MATLAB 2022b

--- MEX_SRC/compile_mex.m
+++ MEX_SRC/compile_mex.m
@@ -10,18 +10,18 @@ switch ext
         warning('unknown mexext %s, assuming 64bit',ext)
         arraydims='-largeArrayDims';
 end
-mkdir('../private');
+mkdir('..\private');
 setenv('CXXFLAGS',[getenv('CXXFLAGS'),' -std=c++11 -O4']);
 if exist('OCTAVE_VERSION','builtin')
-    mex -DOCTAVE -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
-    mex -DOCTAVE -Imatlab_matrix group_handler.cpp matlab_matrix/full.cpp matlab_matrix/sparse.cpp group_index.cpp
-    mex -DOCTAVE ../Assignment/assignmentoptimal.c
+    mex -DOCTAVE -Imatlab_matrix metanetwork_reduce.cpp matlab_matrix\full.cpp matlab_matrix\sparse.cpp group_index.cpp
+    mex -DOCTAVE -Imatlab_matrix group_handler.cpp matlab_matrix\full.cpp matlab_matrix\sparse.cpp group_index.cpp
+    mex -DOCTAVE ..\Assignment\assignmentoptimal.c
 else
-    mex(arraydims,'-Imatlab_matrix','metanetwork_reduce.cpp', 'matlab_matrix/full.cpp', 'matlab_matrix/sparse.cpp', 'group_index.cpp')
-    mex(arraydims,'-Imatlab_matrix', 'group_handler.cpp', 'matlab_matrix/full.cpp', 'matlab_matrix/sparse.cpp', 'group_index.cpp')
-    mex(arraydims,'../Assignment/assignmentoptimal.c')
+    mex(arraydims,'-Imatlab_matrix','metanetwork_reduce.cpp', 'matlab_matrix\full.cpp', 'matlab_matrix\sparse.cpp', 'group_index.cpp')
+    mex(arraydims,'-Imatlab_matrix', 'group_handler.cpp', 'matlab_matrix\full.cpp', 'matlab_matrix\sparse.cpp', 'group_index.cpp')
+    mex(arraydims,'..\Assignment\assignmentoptimal.c')
 end
 
-movefile(['metanetwork_reduce.',ext],['../private/metanetwork_reduce.',ext]);
-movefile(['group_handler.',ext],['../private/group_handler.',ext]);
-movefile(['assignmentoptimal.',ext],['../Assignment/assignmentoptimal.',ext]);
+movefile(['metanetwork_reduce.',ext],['..\private\metanetwork_reduce.',ext]);
+movefile(['group_handler.',ext],['..\private\group_handler.',ext]);
+movefile(['assignmentoptimal.',ext],['..\Assignment\assignmentoptimal.',ext]);
